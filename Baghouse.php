0b927366473909408e22e803ed5fa176dacb2572
xu1998hz <wedxu@ucdavis.edu>
2020-08-05 18:03:39 -0700

    baghouse finalized

--- Baghouse.php
+++ Baghouse.php
@@ -9,21 +9,48 @@
     require_once('sql_task_manager.php');
     $sql_task_manager = new sql_task_manager("localhost", "operator", "Licap123!", "Manufacture");
     # pull out the ID based on last non-null value of baghouse column in table blend
-    $sql_cmd = "SELECT ID FROM blend WHERE BAGHOUSE_WEIGHT IS NOT NULL ORDER BY ID DESC LIMIT 1";
-    $ID_Val = $sql_task_manager->pdo_sql_row_fetch($sql_cmd)['ID'];
-    $sql_weight = "SELECT BAGHOUSE_WEIGHT FROM blend ORDER BY ID DESC LIMIT 1";
-    $Last_Weight = $sql_task_manager->pdo_sql_row_fetch($sql_cmd)['BAGHOUSE_WEIGHT'];
+    $sql_cmd = "SELECT ID, BAGHOUSE_WEIGHT FROM blend WHERE BAGHOUSE_WEIGHT IS NOT NULL ORDER BY ID DESC LIMIT 1";
+    $row_vals = $sql_task_manager->pdo_sql_row_fetch($sql_cmd);
+    $ID_Val = $row_vals['ID']; $Last_Weight = $row_vals['BAGHOUSE_WEIGHT'];
+    $sql_batch = "SELECT BATCH_NUM FROM blend WHERE ID = '$ID_Val' + 1";
+    echo "<H1 style='text-align:center'>BagHouse Page</H1>";
+    echo "<h2 style='text-align:center'>"."Current Batch Number: ".$sql_task_manager->pdo_sql_row_fetch($sql_batch)['BATCH_NUM']."</h2>";
+    $Cur_Date = date("m/d/Y");
+    $sql_real_time = "SELECT BATCH_NUM, MIXING_OP, BAGHOUSE_WEIGHT FROM blend WHERE DATE = '$Cur_Date' ORDER BY ID DESC";
+    $sql_results = $sql_task_manager->pdo_sql_rows_fetch($sql_real_time, array('BATCH_NUM', 'MIXING_OP', 'BAGHOUSE_WEIGHT'));
    ?>
 
    <form action="Baghouse.php" method="post">
-     <H1>BagHouse Page</H1>
-     <p>
+     <p style="text-align:center">
        <label for="BAGHOUSE_WEIGHT">BagHouse Weight</label>
        <input type="text" name="BAGHOUSE_WEIGHT" value="<?php echo htmlentities($Last_Weight); ?>"/>
+       <div style="text-align:center">
        <input type="submit" value="Submit">
+       </div>
      </p>
      <hr>
    </form>
+
+   <script type="text/javascript">
+     var sql_arr = <?php echo json_encode($sql_results) ?>;
+     document.open();
+     document.write("<h1 style='text-align:center'>Baghouse records display Table</h1>");
+     document.write("<h2 style='text-align:center'>This table will display the daily baghouse information</h2>");
+     document.write("<table style='width:100%'>");
+     document.write("<tr> <th>Batch Number</th> <th>Mix Operator</th> <th>Baghouse Weight</th> </tr>");
+     for (i=0; i<sql_arr.length; i++) {
+       var line_str = "<tr>";
+       for (j=0; j<sql_arr[i].length; j++) {
+         line_str+="<td style='text-align:center'>";
+         line_str+=sql_arr[i][j];
+         line_str+="</td>";
+       }
+       line_str += "</tr>";
+       document.write(line_str);
+     }
+     document.write("</table>");
+     document.close();
+   </script>
 
    <?php
     if (count($_REQUEST)!==0) {
