82630afd9edcdab3f701e4ff1bf1b74a7a66fd3b
xu1998hz <wedxu@ucdavis.edu>
2020-08-12 17:26:44 -0700

    real time laminator done

--- realtime_laminator.php
+++ realtime_laminator.php
@@ -1,63 +1,87 @@
 <!DOCTYPE html>
 <html>
 <body>
+  <form action="realtime_laminator.php" method="post">
+    <h1 style='text-align:center'>Real Time Laminator Page</h1>
+    <p style='text-align:center'>
+      <label for='LOWER_DATE'>Begin Query Date:</label>
+      <input id="LOWER_DATE" name="LOWER_DATE" type="date">
+    </p>
+    <p style='text-align:center'>
+      <label for='UPPER_DATE'>End Query Date:</label>
+      <input id="UPPER_DATE" name="UPPER_DATE" type="date">
+    </p>
+
+    <div style="text-align:center">
+    <input type="submit" value="Download CSV" name="Submit_CSV">
+    </div>
+
+    <br/>
+
+    <div style="text-align:center">
+    <input type="submit" value="Display Records" name="Submit_Display">
+    </div>
+    <br/><br/>
+  </form>
+
 <?php
-
-$link = mysqli_connect("localhost", "root", "PQch782tdk@@", "Manufacture");
-// Check connection
-if($link->connect_errno){
-    die('ERROR: Could not connect. ' . $link->connect_error);
-
-}
-//escape strings
-$ELECTRODE_LENGTH = mysqli_real_escape_string($link, $_REQUEST['ELECTRODE_LENGTH']);
-$NUM_DEFECT = mysqli_real_escape_string($link, $_REQUEST['NUM_DEFECT']);
-$AVG_THICKNESS = mysqli_real_escape_string($link, $_REQUEST['AVG_THICKNESS']);
-$TIMESTAMP = mysqli_real_escape_string($link, $_REQUEST['TIMESTAMP']);
-$DATE = date("m/d/Y");
-$TIMESTAMP = date("m/d/Y-H:i:s");
-$ELECTRODE_SERIAL = mysqli_real_escape_string($link, $_REQUEST['ELECTRODE_SERIAL']);
-echo "Current Time: " . $TIMESTAMP . "<br>";
-echo "Time Stamp &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Electrode Serial &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Length &nbsp Avg Thickness &nbsp&nbspNumber of Defects <br>";
-//Pulls data for query
-$myquery = "SELECT ELECTRODE_SERIAL, ELECTRODE_LENGTH, NUM_DEFECT, AVG_THICKNESS, TIMESTAMP FROM LAMINATOR WHERE LAM_DATE = '$DATE' ORDER BY ID DESC";
-$result = mysqli_query($link, $myquery);
-$count = "SELECT COUNT(ELECTRODE_SERIAL) FROM LAMINATOR WHERE LAM_DATE = '$DATE'";
-$count_result = mysqli_query($link, $count);
-$count_count = mysqli_fetch_array($count_result);
-WHILE($row = mysqli_fetch_assoc($result))
-{
-	echo $row['TIMESTAMP']." |&nbsp";
-	echo $row['ELECTRODE_SERIAL']." |&nbsp&nbsp&nbsp";
-	echo $row['ELECTRODE_LENGTH']."|&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp                 ";
-	echo round($row['AVG_THICKNESS'])."|&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp         ";
-	echo $row['NUM_DEFECT']." "."<br>";
-	$TOTAL_LENGTH = $TOTAL_LENGTH + $row['ELECTRODE_LENGTH'];
-}
-	echo "&nbsp&nbsp&nbsp&nbspTOTAL LENGTH:" . $TOTAL_LENGTH;
-	$AVG = $count_count['COUNT(ELECTRODE_SERIAL)'];
-	$AVG_LENGTH = $TOTAL_LENGTH/$AVG;
-	echo "&nbsp&nbspAVG_LENGTH:" . $AVG_LENGTH;
-
-
-
-// close connection
-
-mysqli_close($link);
-
+  require_once('sql_task_manager.php');
+  $sql_task_manager = new sql_task_manager("localhost", "root", "PQch782tdk@@", "Manufacture");
+  # convert html date format to sql date format
+  $T1 = date('Y-m-d', strtotime($_REQUEST['LOWER_DATE'])); $T2 = date('Y-m-d', strtotime($_REQUEST['UPPER_DATE']));
+  if (isset($_REQUEST['Submit_CSV'])) {
+    $row_names = $sql_task_manager->column_name_table('LAMINATOR');
+    $sql_cmd = "SELECT * FROM LAMINATOR WHERE LAM_DATE >= ? AND LAM_DATE <= ? ORDER BY ID DESC";
+    $sql_task_manager->pdo_sql_vali_execute($sql_cmd, array($T1, $T2));
+    $row_values = $sql_task_manager->rows_fetch($row_names);
+    $file_name = 'LAMINATOR_Record_'.date("m_d_Y_H_i_s").'.csv';
+    header('Content-Type: application/csv');
+    header('Content-Disposition: attachment; filename="'.$file_name.'";');
+    // clean output buffer
+    ob_end_clean();
+    $file = fopen('php://output', 'w');
+    fputcsv($file, $row_names);
+    foreach ($row_values as $row) { fputcsv($file, $row); }
+    fclose($file);
+    // flush buffer
+    ob_flush();
+    // use exit to get rid of unexpected output afterward
+    exit();
+  }
+  if (isset($_REQUEST['Submit_Display'])) {
+    $lam_query = "SELECT ELECTRODE_SERIAL, ELECTRODE_LENGTH, NUM_DEFECT, AVG_THICKNESS, TIMESTAMP FROM LAMINATOR WHERE LAM_DATE >= ? AND LAM_DATE <= ? ORDER BY ID DESC";
+    $sql_task_manager->pdo_sql_vali_execute($lam_query, array($T1, $T2));
+    $sql_results = $sql_task_manager->rows_fetch(array('TIMESTAMP', 'ELECTRODE_SERIAL', 'ELECTRODE_LENGTH', 'AVG_THICKNESS', 'NUM_DEFECT'));
+  }
 ?>
-<form action="csv_export_lam.php" method="get">
-  <p>
-    <label for="T1">Begin Time:</label>
-    <input type="text" name="T1" id="T1">
-    <label for="T1">Month/Day/Year Ex: 05/04/2020</label>
-  </p>
-  <p>
-    <label for="T2">End Time:</label>
-    <input type="text" name="T2" id="T2">
-    <label for="T2">Month/Day/Year</label>
-  </p>
-  <input type="submit" value="Download CSV">
-</form>
+
+<script type="text/javascript">
+  var sql_arr = <?php echo json_encode($sql_results) ?>;
+  var total_length = 0;
+  document.open();
+  document.write("<h1 style='text-align:center'>Laminator records display Table</h1>");
+  document.write("<h2 style='text-align:center'>This table will display the records specified by your input dates</h2>");
+  document.write("<table style='width:100%'>");
+  document.write("<tr> <th>Time Stamp</th> <th>Electrode Serial</th> <th>Length</th> <th>Avg Thickness</th> <th>Number of Defects</th></tr>");
+  for (i=0; i<sql_arr.length; i++) {
+    var line_str = "<tr>";
+    for (j=0; j<sql_arr[i].length; j++) {
+      if (j === 2) {
+        total_length += parseInt(sql_arr[i][j]);
+      }
+      line_str+="<td style='text-align:center'>";
+      line_str+=sql_arr[i][j];
+      line_str+="</td>";
+    }
+    line_str += "</tr>";
+    document.write(line_str);
+  }
+  document.write("</table>");
+  var avg_length = total_length/sql_arr.length;
+  document.write("<h3 style='text-align:center'> Total Length: " + total_length + "</h3>");
+  document.write("<h3 style='text-align:center'> Average Length: " + avg_length + "</h3>");
+  document.close();
+</script>
+
 </body>
 </html>
